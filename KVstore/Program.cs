a7bd2ef8dd4c510a54ac92f2366d97aa6cd6e7de
Akshay Mittal <akshaycanodia@gmail.com>
2017-12-09 15:36:01 -0600

    Implement GET, Get by id, Post, Put, Delete

--- KVstore/Program.cs
+++ KVstore/Program.cs
@@ -8,20 +8,74 @@ namespace KVstore
     {
         static void Main(string[] args)
         {
-            string baseAddress = "http://localhost:9000/";
+            string baseAddress1 = "http://localhost:9001/";
+            string baseAddress2 = "http://localhost:9002/";
+            string baseAddress3 = "http://localhost:9003/";
+            string baseAddress4 = "http://localhost:9004/";
+            string baseAddress5 = "http://localhost:9005/";
 
-            // Start OWIN host 
-            using (WebApp.Start<Startup>(url: baseAddress))
+            // Start OWIN host1 
+            using (WebApp.Start<Startup>(url: baseAddress1))
             {
                 // Create HttpCient and make a request to api/values 
-                HttpClient client = new HttpClient();
+                HttpClient client1 = new HttpClient();
 
-                var response = client.GetAsync(baseAddress + "api/values").Result;
+                var response = client1.GetAsync(baseAddress1 + "api/values").Result;
 
                 Console.WriteLine(response);
                 Console.WriteLine(response.Content.ReadAsStringAsync().Result);
                 Console.ReadLine();
             }
+            // Start OWIN host2 
+            using (WebApp.Start<Startup>(url: baseAddress2))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client2 = new HttpClient();
+
+                var response = client2.GetAsync(baseAddress2 + "api/values").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+            }
+            // Start OWIN host3 
+            using (WebApp.Start<Startup>(url: baseAddress3))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client3 = new HttpClient();
+
+                var response = client3.GetAsync(baseAddress3 + "api/values").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+            }
+            // Start OWIN host4 
+            using (WebApp.Start<Startup>(url: baseAddress4))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client4 = new HttpClient();
+
+                var response = client4.GetAsync(baseAddress4 + "api/values").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+            }
+            // Start OWIN host5 
+            using (WebApp.Start<Startup>(url: baseAddress5))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client5 = new HttpClient();
+
+                var response = client5.GetAsync(baseAddress5 + "api/values").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+            }
+
+
         }
     }
 }
--- KVstore/ValuesController.cs
+++ KVstore/ValuesController.cs
@@ -1,35 +1,65 @@
 ﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 
 namespace KVstore
 {
     public class ValuesController : ApiController
     {
+        static Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+
         // GET api/values 
-        public IEnumerable<string> Get()
+        public Dictionary<int, string> Get()
         {
-            return new string[] { "value1", "value2" };
+            //return new string[] { "value1", "value2" };
+            return dictionary;
         }
 
         // GET api/values/5 
         public string Get(int id)
         {
-            return "value";
+
+            string result;
+            if (dictionary.TryGetValue(id, out result))
+            {
+                return result;
+            }
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.NotFound, "Not Found");
+            return response.ToString();
+
         }
 
         // POST api/values 
-        public void Post([FromBody]string value)
+        public string Post([FromBody]string value)
         {
+            int count = dictionary.Count;
+            dictionary.Add(count + 1, value);
+            HttpResponseMessage Sucessresponse = Request.CreateResponse(HttpStatusCode.Created, "Created Sucessfully");
+            return Sucessresponse.ToString();
         }
 
         // PUT api/values/5 
-        public void Put(int id, [FromBody]string value)
+        public string Put(int id, [FromBody]string value)
         {
+            HttpResponseMessage Sucessresponse = Request.CreateResponse(HttpStatusCode.OK, "Updated Sucessfully");
+            dictionary[id] = value;
+            return Sucessresponse.ToString();
         }
 
         // DELETE api/values/5 
-        public void Delete(int id)
+        public string Delete(int id)
         {
+
+            if (dictionary.ContainsKey(id))
+            {
+                dictionary.Remove(id);
+                HttpResponseMessage Sucessresponse = Request.CreateResponse(HttpStatusCode.OK, "Deleted Sucessfully");
+                return Sucessresponse.ToString();
+            }
+            return HttpStatusCode.NotFound.ToString();
         }
     }
 }
