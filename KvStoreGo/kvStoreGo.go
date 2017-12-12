435c93c7a386deffef5521771ede96d8201bca1d
Akshay Mittal <akshaycanodia@gmail.com>
2017-12-12 10:36:55 -0600

    implement put request

--- KvStoreGo/kvStoreGo.go
+++ KvStoreGo/kvStoreGo.go
@@ -6,6 +6,9 @@ import (
 "net/http"
 "os"
 
+	"strings"
+
+	"io"
 )
 func main() {
 	response, err := http.Get("http://localhost:9001/api/values/")
@@ -18,5 +21,25 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(string(responseData))
+}
+
+
+func putRequest(url string, data io.Reader)  {
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodPut, url, data)
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
+	_, err = client.Do(req)
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
+
+}
+
+func httpPutExample()  {
+	putRequest("http://localhost:9001/api/values/2", strings.NewReader("foo"))
 }
 
