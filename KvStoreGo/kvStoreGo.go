ed05e70c55eca0bcc6e9d9901d4652d38beff861
Akshay Mittal <akshaycanodia@gmail.com>
2017-12-12 23:47:41 -0600

    sloppy quorum algo for Qr and Qw

--- KvStoreGo/kvStoreGo.go
+++ KvStoreGo/kvStoreGo.go
@@ -22,18 +22,18 @@ func main() {
 	addrs := []string{":9002", ":9003", ":9004", ":9005"}
 	// Setup a HTTP client
 	c := &http.Client{}
-
+	// Send the two requests
+	//c.Do(putRequest)
+	c.Do(postRequest)
 	//if one node1 write then do write for other nodes
 	if countW<=5  {
 		for _, addr := range addrs{
 			postRequest := newValueRequest(http.MethodPost, "http://localhost"+addr+"/api/values", "bar")
+			//write operation for other nodes
 			c.Do(postRequest)
 			countW++
 		}
 	}
-	// Send the two requests
-	//c.Do(putRequest)
-	c.Do(postRequest)
 
 	response, err := http.Get("http://localhost:9001/api/values")
 	if err != nil {
@@ -68,7 +68,6 @@ func main() {
 
 }
 
-
 func newValueRequest(method, rawurl, value string) *http.Request {
 	b, err := json.Marshal(value)
 	if err != nil {
@@ -85,3 +84,58 @@ func newValueRequest(method, rawurl, value string) *http.Request {
 	return req
 }
 
+/*
+Algorithm for sloppy quorum:
+use vectorclock
+
+func SloppyQuorum(id, nodes)  {
+read the current node, pass nodes in array W/R
+acknowledge
+setTime to run the node
+
+func SloppyQuorum.Write(key, value, write)
+{
+{
+  if(!key || !value || !writeFactor) {
+    throw new Error('write() requires a key, value and a write factor.')
+  }
+  if(!value.clock) {
+    throw new Error('write() value must contain a vector clock.');
+  }
+
+  VClock.increment(value, this.id);
+
+  this._execute({
+      op: 'write',
+      key: key,
+      value: value
+    }, writeFactor, function(err, responses) {
+      callback(err);
+    });
+};
+}
+
+SloppyQuorumRead(key, readFactor, callback) {
+  if(!key || !readFactor) {
+    throw new Error('write() requires a key and a write factor.');
+  }
+  this._execute({
+    op: 'read',
+    key: key
+  }, readFactor, function(err, responses) {
+    // read repair using vector clocks
+    // sort the responses by the vector clocks
+    // then compare them to the topmost (in sequential order, the greatest) item
+      // if they are concurrent with that item, then there is a conflict
+      // that we cannot resolve, so we need to return the item.
+      if(VClock.isConcurrent(item, repaired[0]) && !VClock.isIdentical(item, repaired[0])) {
+        repaired.push(item);
+      } else {
+        // console.log('filtering', item, VClock.isConcurrent(item, repaired[0]), !VClock.isIdentical(item, repaired[0]));
+      }
+    });
+    // combine with own read
+    //repaired
+  });
+};
+*/
